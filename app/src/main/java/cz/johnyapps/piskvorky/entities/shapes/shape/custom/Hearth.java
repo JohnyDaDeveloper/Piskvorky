303efd7e5fa15c765e7073ea5922d970004c7caf
Johny Da Developer <johnydadeveloper@gmail.com>
2020-04-25 23:11:54 +0200

    Oprava ID tvarů

--- app/src/main/java/cz/johnyapps/piskvorky/entities/shapes/shape/custom/Hearth.java
+++ app/src/main/java/cz/johnyapps/piskvorky/entities/shapes/shape/custom/Hearth.java
@@ -3,7 +3,7 @@ package cz.johnyapps.piskvorky.entities.shapes.shape.custom;
 import cz.johnyapps.piskvorky.R;
 
 public class Hearth extends CustomShape {
-    public static final int ID = 4;
+    public static final int ID = 3;
 
     public Hearth() {
         super(R.drawable.hearth);
--- app/src/main/java/cz/johnyapps/piskvorky/entities/shapes/shape/custom/Paw.java
+++ app/src/main/java/cz/johnyapps/piskvorky/entities/shapes/shape/custom/Paw.java
@@ -3,7 +3,7 @@ package cz.johnyapps.piskvorky.entities.shapes.shape.custom;
 import cz.johnyapps.piskvorky.R;
 
 public class Paw extends CustomShape {
-    public static final int ID = 5;
+    public static final int ID = 4;
 
     public Paw() {
         super(R.drawable.paw);
