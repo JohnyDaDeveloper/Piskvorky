ae06f8d9a1e74ad0dc25bc8b50b69093bab58866
Johny Da Developer <johnydadeveloper@gmail.com>
2020-04-25 22:38:36 +0200

    Úklid

--- app/src/main/java/cz/johnyapps/piskvorky/ui/piskvorky/PiskvorkyFragment.java
+++ app/src/main/java/cz/johnyapps/piskvorky/ui/piskvorky/PiskvorkyFragment.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
